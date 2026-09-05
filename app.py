c1029674f482fd2842b50c92b8716076c1cff34f
heliskyJpg <hmendezm6@miumg.edu.gt>
2026-09-05 12:08:20 -0600

    fix: redireccion a index en app.py

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return 'index.html'
+    return render_template('index.html')
 
 
+# archivo = open('clientes.txt', 'w') # 'w' = write (crea o sobreescribe)
+# archivo.write('Ana Lopez\n')
+# archivo.write('Carlos Perez\n')
+# archivo.close()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
