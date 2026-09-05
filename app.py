f9813e621de0deded12815bd9b9a001b829f0323
heliskyJpg <hmendezm6@miumg.edu.gt>
2026-09-05 12:41:21 -0600

    feat: agrega redireccion a registro agende get

--- app.py
+++ app.py
@@ -17,5 +17,19 @@ def inicio():
 # archivo.write('Carlos Perez\n')
 # archivo.close()
 
+@app.route("/registro", methods=["GET", "POST"])
+def registro_colaborador():
+    if request.method =="POST" :
+        pass
+    else:
+        return render_template('registro_agenda.html')
+    
+
+# @app.route('/clientes/listado')
+# def listado_clientes():
+#     with open('clientes.txt', 'r') as f:
+#     nombres = [linea.strip() for linea in f] # una lista con cada nombre
+#     return render_template('clientes_listado.html', nombres=nombres)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
